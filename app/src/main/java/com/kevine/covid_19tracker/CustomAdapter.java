80e4855db1b06e5c2369ae55dc623cc8a5648657
KevineMusoni <kevinenicky@gmail.com>
2020-06-05 22:57:52 -1200

    ...

--- app/src/main/java/com/kevine/covid_19tracker/CustomAdapter.java
+++ app/src/main/java/com/kevine/covid_19tracker/CustomAdapter.java
@@ -1,5 +1,29 @@
 package com.kevine.covid_19tracker;
 
-public class CustomAdapter {
-    
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomAdapter extends ArrayAdapter<Country> {
+
+    private Context context;
+    private List<Country> countryList;
+    private List<Country> countryFiltered;
+
+    public CustomAdapter(@NonNull Context context, List<Country> countryList) {
+        super(context, resource);
+    }
 }
